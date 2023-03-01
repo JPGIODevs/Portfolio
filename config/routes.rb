343e1f76f2db1205d537d8155f1ddbcd7776674f
Jack Gio Packham <jpgiodevelopments@gmail.com>
2023-03-01 19:51:36 +0000

    Path-creating + Navbar button routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   
   get 'home/about'
+  get 'home/resume'
+  get 'home/contacts'
+  #get 'blog/'
+  #get 'portfolio/'
   root 'home#index' #<< created a root for home on normal home rather than home/index
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
