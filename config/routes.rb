54f080c9204d9c48c8a394bd17fad5bd44804e97
JPGIODevs <jpgiodevelopments@gmail.com>
2023-03-06 21:16:36 +0000

    SCSS & contacts form SEMIWORKING

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  
+
   get 'home/about'
   get 'home/resume'
-  get 'home/contacts'
+  #get 'home/contacts'
   #get 'blog/'
   #get 'portfolio/'
-  root 'home#index' #<< created a root for home on normal home rather than home/index
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :contacts, only: [:new, :create ]
+  get '/contacts', to: 'contacts#new', as: 'contact'
+  get 'contacts/sent'
+
+
+  root 'home#index' #<< created a root for home on normal home rather than home/index
+
 
 end
