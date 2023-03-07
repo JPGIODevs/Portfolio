03a93fd7d1f7580b8ad8e5b01d911e16307c6322
JPGIODevs <jpgiodevelopments@gmail.com>
2023-03-07 19:18:43 +0000

    unfun changes + contacts addition

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,9 @@ Rails.application.routes.draw do
 
   get 'home/about'
   get 'home/resume'
-  #get 'home/contacts'
+  get 'home/contacts'
   #get 'blog/'
   #get 'portfolio/'
-
-  resources :contacts, only: [:new, :create ]
-  get '/contacts', to: 'contacts#new', as: 'contact'
-  get 'contacts/sent'
 
 
   root 'home#index' #<< created a root for home on normal home rather than home/index
