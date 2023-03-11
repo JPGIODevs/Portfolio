5b7a8336932e0404df348abdda15f94f72352208
JPGIODevs <jpgiodevelopments@gmail.com>
2023-03-11 11:36:14 +0000

    css styling

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,7 +6,7 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
         if (entry.isIntersecting){
-            entry.target.classList.add('show');
+            entry.target.classList.toggle('show', entry.isIntersecting);
         } else {
             entry.target.classList.remove('show');
         }
