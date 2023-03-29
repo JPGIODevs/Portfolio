ed375cae7ab2dda8762a784831f6c6624d6f8831
JPGIODevs <jpgiodevelopments@gmail.com>
2023-03-29 21:20:24 +0100

    docker change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.require_master_key = true
+  # config.require_master_key = true
+  config.read_encrypted_secrets = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
