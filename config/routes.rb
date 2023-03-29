1a4028fb523c16e1484341bbb4246a7573ec2d5c
JPGIODevs <jpgiodevelopments@gmail.com>
2023-03-29 11:13:37 +0100

    form creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
+  resources :contacts, only: [:new, :create]
+
 #projects app
   get 'projects/portfolio'
   get 'projects/placeholder'
   #unfinishing projects :) route as needed
   get 'projects/manifold' => redirect("projects/placeholder")
+  get 'projects/tauritasks' => redirect("projects/placeholder")
+  get 'projects/ps1horror' => redirect("projects/placeholder")
+  get 'projects/twilioAI' => redirect("projects/placeholder")
+  get 'projects/reader' => redirect("projects/placeholder")
 #home app
-  get 'home/about'
-  get 'home/resume'
   get 'home/contacts'
   root 'home#index' #<< created a root for home on normal home rather than home/index
+#contacts app
+  get 'contacts/new'
 end
