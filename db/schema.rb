7114d32c959ae05de2b9f387bc8f43c766d28bf0
JPGIODevs <jpgiodevelopments@gmail.com>
2023-04-17 18:10:04 +0100

    postgresql conversion

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_04_13_152540) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -30,7 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_13_152540) do
     t.string "commenter"
     t.string "string"
     t.text "body"
-    t.integer "article_id", null: false
+    t.bigint "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
