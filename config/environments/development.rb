e2af6241974e69132ce86a6d0b422ba8e7a0bc68
JPGIODevs <jpgiodevelopments@gmail.com>
2023-04-20 09:56:35 +0100

    aptfile addition (vips)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :amazon
+  Rails.application.config.active_storage.variant_processor = :vips
 
   # Don't care if the mailer can't send.
 
