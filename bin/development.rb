9fe847052448e00b078b0f8fa6314ae7602129e4
JPGIODevs <jpgiodevelopments@gmail.com>
2023-04-22 11:35:31 +0100

    security change + dev env

--- bin/development.rb
+++ bin/development.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
 
-  # Print deprecation notices to the Rails logger
+  # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
   # Raise exceptions for disallowed deprecations.
