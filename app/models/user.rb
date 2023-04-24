ee2dab1a92f9ba7c1d79e1e55c662c93253cc818
JPGIODevs <jpgiodevelopments@gmail.com>
2023-04-24 11:10:03 +0100

    webkit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   # OMMITED REGISTERABLE
-  devise :database_authenticatable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 end
