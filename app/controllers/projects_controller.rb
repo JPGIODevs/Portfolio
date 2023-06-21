273ac5a2a9a0b2d9b4499e7807c4fcf5d257866a
JPGIODevs <jpgiodevelopments@gmail.com>
2023-06-21 18:21:19 +0100

    project change

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,8 @@
 class ProjectsController < ApplicationController
-  def portfolio
-  end
+  def update_stream
+    case params[:pageId]
+      when 1
+        replace_stream("placeholder", "projects/home")
+      when 2 
+        replace_stream("portfolio", "projects")
 end
